faafb94583eb10387cc0a63dc5749c286b963003
ameotoko <andrey.vinichenko@gmail.com>
2022-08-18 20:47:33 +0200

    add backend wildcard for content element

--- src/CountdownContent.php
+++ src/CountdownContent.php
@@ -6,11 +6,16 @@
 
 namespace Ameotoko\Countdown;
 
+use Contao\Config;
 use Contao\ContentModel;
 use Contao\CoreBundle\Controller\ContentElement\AbstractContentElementController;
+use Contao\CoreBundle\Routing\ScopeMatcher;
 use Contao\CoreBundle\ServiceAnnotation\ContentElement;
+use Contao\Date;
+use Contao\StringUtil;
 use Contao\Template;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -19,9 +24,32 @@ use Symfony\Component\HttpFoundation\Response;
 class CountdownContent extends AbstractContentElementController
 {
     public const TYPE = 'countdown';
+    private RequestStack $requestStack;
+    private ScopeMatcher $scopeMatcher;
+
+    public function __construct(RequestStack $requestStack, ScopeMatcher $scopeMatcher)
+    {
+        $this->requestStack = $requestStack;
+        $this->scopeMatcher = $scopeMatcher;
+    }
 
     protected function getResponse(Template $template, ContentModel $model, Request $request): ?Response
     {
+        if ($this->scopeMatcher->isBackendRequest($this->requestStack->getCurrentRequest())) {
+            return $this->getWildcard($model);
+        }
+
         return $template->getResponse();
+    }
+
+    protected function getWildcard(ContentModel $model): Response
+    {
+        $data = StringUtil::deserialize($model->headline);
+
+        return $this->render('@AmeotokoCountdown/be_countdown.html.twig', [
+            'headline' => \is_array($data) ? $data['value'] : $data,
+            'tstamp' => Date::parse(Config::get('datimFormat'), $model->endDate),
+            'expired' => $model->endDate < time()
+        ]);
     }
 }
